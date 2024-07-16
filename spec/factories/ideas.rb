bff61cc506dd2dd1ce6341ff2089defadf157862
ingarondel <inga404@icloud.com>
2024-07-16 06:43:38 -0700

    feat: factory for idea

---
+++ spec/factories/ideas.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :idea do
+    title            { "MyString" }
+    description      { "MyText" }
+    fund_capital     { 100 }
+    industries       { "Industry" }
+    problem_solved   { "Solved" }
+    geographic_focus { "Europe"}
+    team_members     { "1 CTO" }
+    next_steps       { "Next" }
+    investor_require { "Requirement" }
+
+    trait :invalid do
+      title { nil }
+    end
+  end
+end
