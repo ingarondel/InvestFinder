b7654bc331cfd2b00d9d6158c7b4d7bb1bb52f7f
ingarondel <inga404@icloud.com>
2024-07-18 02:18:42 -0700

    feat: add associations for idea model and spec

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,6 @@
 class Idea < ApplicationRecord
+  belongs_to :user
+
   validates :title,          :description,      :fund_capital, :industries,
             :problem_solved, :geographic_focus, :team_members,
             :next_steps,     :investor_require, presence: true
