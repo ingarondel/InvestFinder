7068d9c52ed34fee43cdf80c1fe52a358daf93c5
ingarondel <inga404@icloud.com>
2024-07-18 05:00:42 -0700

    fix: add user id and user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+user = User.create([
+{
+	name: 'Admin',
+	surname: 'Admin',
+	email: 'admin@admin.com',
+	password: '123456',
+	confirmed_at: Time.now
+}
+])
+
 ideas = Idea.create([
   {
 	title: 		 	  Faker::Lorem.sentence(word_count: 3),
@@ -9,6 +19,7 @@ ideas = Idea.create([
 	team_members: 	  '1 CEO' ,
 	next_steps: 	  Faker::Lorem.sentence,
 	investor_require: Faker::Lorem.sentence,
+	user_id: 1
   },
 {
 	title: 		 	  Faker::Lorem.sentence(word_count: 3),
@@ -20,6 +31,7 @@ ideas = Idea.create([
 	team_members: 	  '3 CEO' ,
 	next_steps: 	  Faker::Lorem.sentence,
 	investor_require: Faker::Lorem.sentence,
+	user_id: 1
   },
   {
 	title: 		 	  Faker::Lorem.sentence(word_count: 3),
@@ -31,5 +43,6 @@ ideas = Idea.create([
 	team_members: 	  '1 CEO, 2 CTO' ,
 	next_steps: 	  Faker::Lorem.sentence,
 	investor_require: Faker::Lorem.sentence,
+	user_id: 1
   }
 ])
