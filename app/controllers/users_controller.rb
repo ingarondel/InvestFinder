cd0ce73b03f2fa93f03af56efcf69ad532712832
ingarondel <inga404@icloud.com>
2024-07-19 01:27:48 -0700

    feat: controller for user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :destroy]
+
+  def show
+    @user = User.find(params[:id])
+    @ideas = @user.ideas
+  end
+
+  def destroy
+    sign_out(current_user)
+    redirect_to root_path
+  end
 end
