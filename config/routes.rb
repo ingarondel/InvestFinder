e2a96ad77545aa891c535f22f945791694135a42
ingarondel <inga404@icloud.com>
2024-07-22 02:46:51 -0700

    feat: responses to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
    devise_for :users, controllers: {
      registrations: 'registrations/registrations'
    }
-  resources :ideas
+  resources :ideas do
+    resources :responses, only: [:create, :destroy]
+  end
   resources :users
 
   root to: 'ideas#index'
