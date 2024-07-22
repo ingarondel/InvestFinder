48910bf6c21a73f210d44bc486941f8de94fe3ba
ingarondel <inga404@icloud.com>
2024-07-22 02:44:30 -0700

    fix: user spec model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many(:ideas)}
+  it { should have_many(:ideas).dependent(:destroy)}
 
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
