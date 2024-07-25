e37ab2aaa33646bfccc044ec3c36b863760901cf
ingarondel <inga404@icloud.com>
2024-07-25 01:10:08 -0700

    fix: show for not investor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @ideas = @user.ideas
-    @contact = @user.contact
+    @contact = @user.contact if @user.type == 'Investor'
   end
 
   def destroy
