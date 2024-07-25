eb9348f1e475bad7a52f03b3ddaf3889838d6b1b
ingarondel <inga404@icloud.com>
2024-07-25 01:30:45 -0700

    feat: add comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_083755) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_25_081050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "idea_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "phone"
@@ -76,6 +86,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_083755) do
     t.index ["type"], name: "index_users_on_type"
   end
 
+  add_foreign_key "comments", "ideas"
+  add_foreign_key "comments", "users"
   add_foreign_key "contacts", "users", column: "investor_id"
   add_foreign_key "responses", "contacts"
   add_foreign_key "responses", "ideas"
